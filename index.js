b1da313c20d935f971cec4797529ebae67187f4c
pedroaraujox <pa8088253@gmail.com>
2024-09-24 19:48:30 -0300

    codigo pronto

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-// Variáveis
-let nomeHeroi = "teste";
-let xpHeroi = 11000;
-let nivel;
+let nome = "pedrinavassalador";
+let xp = 6500;
+let level;
 
-// Estruturas de Decisão
-if (xpHeroi < 1000) {
-    nivel = "Ferro";
-} else if (xpHeroi <= 2000) {
-    nivel = "Bronze";
-} else if (xpHeroi <= 5000) {
-    nivel = "Prata";
-} else if (xpHeroi <= 7000) {
-    nivel = "Ouro";
-} else if (xpHeroi <= 8000) {
-    nivel = "Platina";
-} else if (xpHeroi <= 9000) {
-    nivel = "Ascendente";
-} else if (xpHeroi <= 10000) {
-    nivel = "Imortal";
+if (xp < 1000) {
+    level = "Ferro";
+} else if (xp <= 2000) {
+    level = "Bronze";
+} else if (xp <= 5000) {
+    level = "Prata";
+} else if (xp <= 7000) {
+    level = "Ouro";
+} else if (xp <= 8000) {
+    level = "Platina";
+} else if (xp <= 9000) {
+    level = "Ascendente";
+} else if (xp <= 10000) {
+    level = "Imortal";
 } else {
-    nivel = "Radiante";
+    level = "Radiante";
 }
 
-// Saída
-console.log(`O herói de nome ${nomeHeroi} está no nível ${nivel}`);
+
+console.log(" O HEROI DE NOME " + nome + " esta no nivel " + level);
